330443b8d41f3087474fc414d639e9c2a40c2150
Vibhuti Arora <vibhutiarora355@gmail.com>
2021-08-15 19:59:48 +0530

    Homepage and Directory Components

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './App.css';
+import HomePage from './pages/homepage/homepage.component';
+
+
+function App() {
+    return ( <
+        div >
+        <
+        HomePage / >
+        <
+        /div>
+
+    );
+
+
+}
+
+export default App;
